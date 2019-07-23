e701eb22da861c9020c55653b733aa863f336f3a
Kelley Chaplain <kmchaplain@gmail.com>
2019-07-23 18:28:34 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -17,6 +18,7 @@ class InteractiveRecord
       column_names << column["name"]
     end
     column_names.compact
+    # binding.pry
   end 
   
   def initialize(options={})
@@ -36,16 +38,21 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless
-    send(col_name).nil?    
+      values << "'#{send(col_name)}'" unless send(col_name).nil?    
     end
     values.join(", ")  
   end
   
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(llamo)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{llamo}'"
+    DB[:conn].execute(sql)
   end  
   
 end
